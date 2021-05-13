b0f9a4a97918b14eab454dc5745e2d09ed842821
ladyomega <68204412+ladyomega@users.noreply.github.com>
2021-05-13 15:56:49 -0400

    make-a-thon_adm
    Honors Project V1.0

--- mentor.cpp
+++ mentor.cpp
@@ -1,3 +1,11 @@
+/*
+Thamires Lopes de Andrade
+EMPLID: 23949613
+CSC 221H-1700 [Spring 2021]
+Submitted: May 13th, 2021
+Time Stamp: 5:30pm
+Prof: Dr. Azhar
+*/
 #include "mentor.h"
 
 Mentor::Mentor(int id, string firstName, string lastName, string email, string company, string topic, int group_id) : Person(id, firstName, lastName, email, topic, group_id)
